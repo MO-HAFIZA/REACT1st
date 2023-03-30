295823753cc78442afbabc584c699515f73e81c9
mohamed abdo <mohamedhafiza1234@gmail.com>
2023-03-30 14:34:23 +0200

    yes

---
+++ myapp/src/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <html>
+      <head>
+        <title>c4a</title>
+      </head>
+      <body>
+      <h1 className='hi'>hiiiiiiiiiiiiiiiiii 😍🤩 </h1>
+      </body>
+    </html>
+  )
+}
